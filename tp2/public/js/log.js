0a51264d2754aa85f83ed80fed98e6204b7e4189
brduarte <bryan.duarte@etu.uca.fr>
2021-02-15 16:52:09 +0100

    tp2

--- tp2/public/js/log.js
+++ tp2/public/js/log.js
@@ -2,3 +2,17 @@ function logMessageWithDate(message){
 	let today = new Date();
 	console.log(today.toLocaleString() + ' : ' + message );
 }
+
+function displayMessageFromArray(array){
+	array.forEach(element => logMessageConsole(element));
+}
+
+function logMessageConsole(message){
+  console.log(getMessageFormatted(message));
+}
+
+function getMessageFormatted(message){
+  let messageFormatted = "Message : " + message;
+
+  return messageFormatted;
+}
--- tp2/public/js/main.js
+++ tp2/public/js/main.js
@@ -6,10 +6,12 @@ let a = new Article("oui",55,"caca");
 a.addArticle();
 
 let json = JSON.parse(ALLNEWSJSON);
+logMessageConsole(a);
 console.log(json);
 
 json.forEach(item => {
   let a = new Article(item.title,item.id,item.desc);
   a.addArticle();
+  logMessageConsole(a);
   console.log(a);
 });
